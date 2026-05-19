8295590cf78f371910d3fca675335e79c930d516
djukes-byupathway <djukes@byupathway.edu>
2026-05-18 19:00:15 -0500

    chamber directory list buttons

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,48 +1,73 @@
-const url = 'https://github.com/djukes-byupathway/wdd231/blob/main/chamber/data/members.json';
+
+const url = 'https://djukes-byupathway.github.io/wdd231/chamber/data/members.json';
 const cards = document.querySelector('#cards');
+const gridbutton = document.querySelector("#btnGrid")
+const listbutton = document.querySelector("#btnList")
 
 async function getMemberData() {
     const response = await fetch(url);
     const data = await response.json();
+    console.table(data.members); // temporary testing of data response
     displayMembers(data.members);
-}
+} // end member data
 
 getMemberData();
 
-const displayMembers = (members) => {
-    prophets.forEach((member) => {
-        //create card element 
-        let card = document.createElement('section');
-        // let logo = document.createElement('img');
-        let company = document.createElement('h2');
-        let addr = document.createElement('p');
-        let phone = document.createElement('p');
-        let website = document.createElement('a');
+gridbutton.addEventListener("click", () => {
+    // example using arrow function
+    cards.classList.add("grid");
+    cards.classList.remove("list");
+});
 
-/* 
-        // create logo image element
-        portrait.setAttribute('src', member.imageurl);
-        portrait.setAttribute('alt', `Logo for ${member.company} `);
-        portrait.setAttribute('width', '340');
-        portrait.setAttribute('height', '440'); */
-        
-        //h2
-        company.innerHTML = `${company}`;
 
-        //create address element
-        addr.innerHTML = `${address}`;
-        // create phone number element
-        addr.innerHTML = `${phone}`;
-        //create  url element
-        addr.innerHTML = `${url}`;
+listbutton.addEventListener("click", showList); // example using defined function
 
-        // Append the section(card) with the created elements
-        card.appendChild(company);
-        card.appendChild(addr);
-        card.appendChild(phone);
-        card.appendChild(website);
-
-        cards.appendChild(card);
-    }); // end of arrow function and forEach loop
+function showList() {
+    cards.classList.add("list");
+    cards.classList.remove("grid");
 }
+
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+
+        let card = document.createElement('section');
+        let company = document.createElement('h2');
+        let logo = document.createElement('img');
+        let address = document.createElement('p');
+        let phone = document.createElement('p');
+
+        let web = document.createElement('a');
+
+        let txtAddress = member.address;
+        let txtPhone = member.phone;
+        let txtWebsite = member.url;
+
+
+        //
+        logo.setAttribute('src', member.imageurl);
+        logo.setAttribute('alt', member.company);
+        logo.setAttribute('width', '340');
+        logo.setAttribute('height', '440');
+        //
+        company.innerHTML = member.company;
+        //
+        address.innerHTML = txtAddress;
+        phone.innerHTML = txtPhone
+
+        web.setAttribute('href', `${txtWebsite}`);
+        web.textContent = txtWebsite;
+
+    
+        //
+        card.appendChild(logo);
+        card.appendChild(company);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(web);
+
+        //
+        cards.appendChild(card);
+    });
+} // end display members
 
