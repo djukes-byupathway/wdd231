02c1e0f503493b2a48761ad7e8d1c9a2366a40cf
djukes-byupathway <djukes@byupathway.edu>
2026-06-05 15:36:17 -0500

    modal complete

--- chamber/scripts/memberlevel.js
+++ chamber/scripts/memberlevel.js
@@ -1,7 +1,15 @@
 
 const url = 'https://djukes-byupathway.github.io/wdd231/chamber/data/memberlevel.json';
-
 const cards = document.querySelector('#cardcontainer');
+
+//dialog elements
+const mydialog = document.querySelector("#memberLevel");
+const myClose = document.querySelector("#dialogClose");
+const myTitle = document.querySelector("#myTitle");
+const priceP = document.querySelector("#priceP");
+const beneTitle = document.querySelector("#memberLevel h3");
+const beneList = document.querySelector("#memberLevel ul");
+
 
 async function getMemberLevels() {
     const response = await fetch(url);
@@ -11,16 +19,13 @@ async function getMemberLevels() {
 } // end member data
 
 getMemberLevels();
+mydialog.close();
+
+myClose.addEventListener("click", () => mydialog.close());
 
 const displayLevels = (levels) => {
     levels.forEach((level) => {
 
-        /* 
-        <div class="card">
-                    <h3> Non Profit Membership </h3>
-                    <button>Learn More</button>
-                </div>
-        */
         const card = document.createElement('div');
         card.classList.add('card');
 
@@ -28,13 +33,27 @@ const displayLevels = (levels) => {
         cardHeading.innerHTML = level.name;
 
         let btnDetails = document.createElement('button');
-        btnDetails.textContent = 'View Details'
+        btnDetails.textContent = 'View Details';
         btnDetails.addEventListener('click', () => showDialog(level));
-        
-        console.log(level.name);
+
         card.appendChild(cardHeading);
         card.appendChild(btnDetails);
         cards.appendChild(card);
     });
 } // end display members
 
+
+function showDialog(level) {
+    //set values of existing elements
+    myTitle.innerHTML = level.name;
+    priceP.innerHTML = `Annual Price(USD): ${level.price}`;
+    // add benefits from json
+    beneList.replaceChildren();
+    level.benefits.forEach(benefit => {
+        let beneLI = document.createElement('li');
+        beneLI.innerHTML = benefit;
+        beneList.appendChild(beneLI);
+    });
+   
+    mydialog.showModal();
+}
